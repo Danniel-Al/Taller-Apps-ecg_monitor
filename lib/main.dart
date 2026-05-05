db750d52dd48b69d6a3704b9e9e06ec79240cb22
Daniel Alfaro <A01174359@tec.mx>
2026-05-04 19:48:42 -0600

    Mejora diseño estético de datos de usuarios y constantes

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,29 @@
 // lib/main.dart
 // PUNTO DE ENTRADA PRINCIPAL DE LA APLICACIÓN
-// Este es el primer archivo que se ejecuta al iniciar la app
 
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 
 void main() {
-  // Función principal que inicia la aplicación
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // StatelessWidget = widget que no cambia su estado (estático)
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // MaterialApp = configuración principal de la app
     return MaterialApp(
-      title: 'Monitor ECG',                    // Título visible en la app
-      debugShowCheckedModeBanner: false,       // Oculta la etiqueta "DEBUG" en la esquina
+      title: 'Monitor ECG',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,             // Color principal (rojo = corazón)
-        useMaterial3: true,                   // Usa la versión más reciente de Material Design
+        primarySwatch: Colors.red,
+        useMaterial3: true,
+        fontFamily: 'Roboto',
       ),
-      home: const LoginScreen(),               // Pantalla que se muestra al abrir la app
+      home: const LoginScreen(),
     );
   }
 }
+
 
